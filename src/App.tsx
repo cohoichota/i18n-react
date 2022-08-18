238574817d2465661be79c7022deb07ef2721b58
dlchau <dlchau@cmcglobal.vn>
2022-08-18 15:50:59 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { i18n } from "./translations/i18n";
+import { useTranslation } from "react-i18next";
 
 function App() {
+  const [language, setLanguage] = useState<string>("en");
+  const { t } = useTranslation();
+  let lng = language;
+
+  const handleOnClick = (e: any) => {
+    e.preventDefault();
+    setLanguage(e.target.value);
+    i18n.changeLanguage(e.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <select onClick={handleOnClick}>
+        <option selected value="en">
+          English
+        </option>
+        <option value="es">Spanish</option>
+        <option value="zh">Chinese</option>
+        <option value="hindi">Hindi</option>
+      </select>
+
+      <div>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <p>{t("welcome")}</p>
+          <p>
+            {t("date_format_one", { date: new Date() })} ={" "}
+            {t("keyWithCount", { count: 3 })}
+          </p>
+          <p>{t("number_format", { number: 195 })}</p>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
